ea63601523e919566a82b9db3e747b1b733af81e
ohasis <dhgkdud4002@naver.com>
2021-10-01 14:08:32 +0900

    server 포트번호 5000으로 변경

--- front-end/src/components/views/LoginPage/LoginPage.js
+++ front-end/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginPage(){
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}>
+                <label>Email</label>
+                <input type="email" value={Email} />
+                <label>Password</label>
+                <input type="password" value={Password} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
