3f1ed46afcf61dcf43c9ad1f60e2eb4ef88477b8
silverz <liisa9601@naver.com>
2021-11-24 16:44:28 +0900

    검색기능 구현 완료

--- front-end/src/components/views/LandingPage/LandingPage.js
+++ front-end/src/components/views/LandingPage/LandingPage.js
@@ -8,19 +8,29 @@ const {Title} = Typography
 function LandingPage(){
 
     const [Restaurants, setRestaurants] = useState([])
-    const [MemberList, setMemberList] = useState([])
+    const [MemberList, setMemberList] = useState([1])
     const [Name, setName]=useState('')
-
+    const [btnState, setbtnState] = useState('Name')
     
     const OPTIONS = [
         { value: "name", name: "이름으로 찾기" },
         { value: "place", name: "위치로 찾기" },
 
     ];
+ 
     const SelectBox = (props) => {
         const handleChange = (e) => {
             // event handler
-            console.log(e.target.value);
+           //console.log(e.target.value);
+           if(e.target.value === 'name'){
+               //console.log("이름으로 검색")
+               setbtnState('Name')
+              
+           }
+           else{
+               //console.log("주소로 검색")
+               setbtnState('Address')
+           }
         };
     
         return (
@@ -35,16 +45,19 @@ function LandingPage(){
                     </option>
                 ))}
             </select>
+            
         );
     };
+
+
     const NameHandler=(e)=>{
         setName(e.currentTarget.value)
        
     }
 
-    function searchByName() {
-        
-        Axios.get(`/api/member/searchbyName/${Name}`)
+    function searchBy() {
+        console.log(btnState)
+        Axios.get(`/api/member/${btnState}/${Name}`)
             .then(response => {
                 if(response.data){
                     console.log(response.data)
@@ -57,21 +70,6 @@ function LandingPage(){
             })
         
         }
-
-        function searchByAddress() {
-            Axios.get(`/api/member/searchbyAddress/${Name}`)
-                .then(response => {
-                    if(response.data){
-                        console.log(response.data)
-                        setMemberList(response.data)
-    
-                    }
-                    else{
-                        alert('예약 정보를 가져오는데 실패')
-                    }
-                })
-            
-            }
 
             
     useEffect(() => {
@@ -107,10 +105,13 @@ function LandingPage(){
 
     const renderCards = Restaurants.map((restaurant, index) => {
         //console.log(restaurant)
-        if (MemberList.length === 0){
-          
+        
+        
         return( 
+          
+            
         <Col lg={6} md={8} xs={24} key={index}>
+
             <a href={`/restaurant/${restaurant.memberNo}`}>
                 <div style={{position:'relative', border:'1px solid rgb(232,232,232)',textAlign:'center', padding:'15px 0'}}>
                 {restaurant.memberName}<br/>
@@ -129,15 +130,12 @@ function LandingPage(){
             <span style={{ marginLeft: '3rem' }}> {video.views}</span> - 
             <span> {moment(video.createdAt).format("MMM Do YY")} </span> */}
         </Col>
-
+     
         )
-        }
-        else{
-            return null
-        }
+        
     })
 
-
+console.log(MemberList)
     return (
         <div style={{width:'85%', margin:'3rem auto'}}>
             <Title level={2}>맛집 둘러보기</Title>
@@ -145,8 +143,10 @@ function LandingPage(){
             <SelectBox options={OPTIONS} defaultValue="name"></SelectBox>&ensp;&ensp;
                 <input type="text" onChange={NameHandler}></input>
                 
-                <button onClick={searchByName}>Search</button>
-                <button onClick={searchByAddress}>Search</button>
+                <button onClick={searchBy}>Search</button>
+
+                
+
             </div>
             <hr />
             
@@ -156,12 +156,27 @@ function LandingPage(){
                 
             </Row>
         
-            
-            <Row gutter={[32,16]}>
-
+            {
+                MemberList[0] === 1
+                ?(
+                <Row gutter={[32,16]}>
                 {renderCards}
                 
+            </Row>)
+            : MemberList.length === 0
+                ?(
+                    <Row gutter={[32,16]}>
+                     <div style={{width:'100%', textAlign:'center'}}><h1>검색 결과가 없습니다.</h1></div>
+
+                
+                {renderCards}
+                
+                
             </Row>
+                )
+                :null
+            }
+            
 
 
         </div>
